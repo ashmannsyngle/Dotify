a7c4897d18656f1f0ee5ab8805da66a71fa8903a
Ashmann <ashmann@uw.edu>
2020-04-08 01:35:49 +0530

    almost done, lecture to be seen

--- app/src/main/java/com/example/dotify/MainActivity.kt
+++ app/src/main/java/com/example/dotify/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.dotify
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.core.content.ContextCompat
 import org.w3c.dom.Text
 import kotlin.random.Random
 
@@ -41,21 +40,39 @@ class MainActivity : AppCompatActivity() {
         nextButton.setOnClickListener {nextButton: View ->
             Toast.makeText(this, "Skipping to next track", Toast.LENGTH_SHORT).show()
         }
+
+        val image = findViewById<ImageButton>(R.id.cover)
+        image.setOnLongClickListener {image: View ->
+            val color = resources.getColor(R.color.red)
+            val username = findViewById<TextView>(R.id.username)
+            username.setTextColor(color)
+            val songName = findViewById<TextView>(R.id.song_name)
+            songName.setTextColor(color)
+            val artistName = findViewById<TextView>(R.id.artist_name)
+            artistName.setTextColor(color)
+            val plays = findViewById<TextView>(R.id.no_of_plays)
+            plays.setTextColor(color)
+            true
+        }
     }
 
     fun changeUserClicked(view: View) {
         val changeUsernameButton = findViewById<Button>(R.id.btn_change_user)
         val username = findViewById<TextView>(R.id.username)
-        val editUsername = findViewById<TextView>(R.id.edit_username)
+        val editUsername = findViewById<EditText>(R.id.edit_username)
         if (changeUsernameButton.text != "Apply") {
             username.visibility = View.GONE
             editUsername.visibility = View.VISIBLE
             changeUsernameButton.text = "Apply"
         } else {
             username.text = editUsername.text.toString()
-            editUsername.visibility = View.GONE
-            username.visibility = View.VISIBLE
-            changeUsernameButton.text = "Change User"
+            if (username.text == "") {
+                Toast.makeText(this, "Cannot apply if username is empty!", Toast.LENGTH_SHORT).show()
+            } else {
+                editUsername.visibility = View.GONE
+                username.visibility = View.VISIBLE
+                changeUsernameButton.text = "Change User"
+            }
         }
     }
 }
