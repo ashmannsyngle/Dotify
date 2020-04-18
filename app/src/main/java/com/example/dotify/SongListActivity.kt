75e1bdb684f5c7d5be823f32122a27b5e3305e5b
Ashmann <ashmann@uw.edu>
2020-04-19 02:11:20 +0530

    part 2 done

--- app/src/main/java/com/example/dotify/SongListActivity.kt
+++ app/src/main/java/com/example/dotify/SongListActivity.kt
@@ -2,8 +2,10 @@ package com.example.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.ericchee.songdataprovider.SongDataProvider
 import kotlinx.android.synthetic.main.activity_song_list.*
+import kotlin.random.Random
 
 class SongListActivity : AppCompatActivity() {
 
@@ -12,7 +14,15 @@ class SongListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_song_list)
 
         val listOfSongs = SongDataProvider.getAllSongs()
-        val songAdapter = SongListAdapter(listOfSongs)
+        val songAdapter = SongListAdapter(listOfSongs, this)
+
+        songAdapter.onSongClickListener = { song ->
+            tvPreviewSongTitle.text = getString(R.string.previewText).format(song.title, song.artist)
+        }
+
+        btnShuffle.setOnClickListener{
+            songAdapter.change(listOfSongs.shuffled(Random))
+        }
 
         rvSongs.adapter = songAdapter
     }
--- app/src/main/java/com/example/dotify/SongListAdapter.kt
+++ app/src/main/java/com/example/dotify/SongListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.dotify
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +9,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ericchee.songdataprovider.Song
 
-class SongListAdapter(private val listOfSongs: List<Song>): RecyclerView.Adapter<SongListAdapter.SongViewHolder>() {
+class SongListAdapter(private var listOfSongs: List<Song>, context: Context): RecyclerView.Adapter<SongListAdapter.SongViewHolder>() {
+
+    var onSongClickListener: ((song : Song) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_song, parent, false)
@@ -19,21 +22,27 @@ class SongListAdapter(private val listOfSongs: List<Song>): RecyclerView.Adapter
 
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         val song = listOfSongs[position]
-        val songTitle = song.title
-        val artistName = song.artist
-        val songImage = song.smallImageID
-        holder.bind(songTitle, artistName, songImage)
+        holder.bind(song)
     }
 
-    class SongViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    fun change(newSongs : List<Song>) {
+        listOfSongs = newSongs
+        notifyDataSetChanged()
+    }
+
+    inner class SongViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         private val tvSongTitle by lazy { itemView.findViewById<TextView>(R.id.tvSongTitle) }
         private val tvArtistName by lazy { itemView.findViewById<TextView>(R.id.tvArtistName) }
         private val ivSongImage by lazy { itemView.findViewById<ImageView>(R.id.songImage) }
 
-        fun bind(songTitle: String, artistName: String, songImage: Int) {
-            tvSongTitle.text = songTitle
-            tvArtistName.text = artistName
-            ivSongImage.setImageResource(songImage)
+        fun bind(song: Song) {
+            tvSongTitle.text = song.title
+            tvArtistName.text = song.artist
+            ivSongImage.setImageResource(song.smallImageID)
+
+            itemView.setOnClickListener {
+                onSongClickListener?.invoke(song)
+            }
         }
     }
 }
